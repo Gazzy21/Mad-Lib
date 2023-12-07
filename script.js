9c76195756db1b5a1b352db1216a7362f3592154
Gazzy21 <nicholas221@icloud.com>
2023-12-07 13:46:15 -0500

    fixed backticks

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let noun = prompt('Enter a noun.')
 let verb = prompt('Enter a verb')
 let adverb = prompt('Enter an adverb')
 
-let story = 'Once upon a time, in a ${adjective} ${noun}, a ${verb} ${noun} ${adverb} ${verb}.'
+let story = `Once upon a time, in a ${adjective} ${noun}, a ${verb} ${noun} ${adverb} ${verb}.`
 
 let madLibOutputDiv = document.getElementById('madLibOutput')
 
